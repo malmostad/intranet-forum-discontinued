01cc4f974359ee8d2d7ec18426a470c8d520a739
Mattias Andersson <mattias.h.andersson@gmail.com>
2013-06-17 17:13:36 +0200

    Topic list table using assets table

--- sites/all/themes/malmostad/malmostad_forum/templates/advanced-forum-topic-list-view.tpl.php
+++ sites/all/themes/malmostad/malmostad_forum/templates/advanced-forum-topic-list-view.tpl.php
@@ -19,41 +19,43 @@
     <caption><?php print $title; ?></caption>
   <?php endif; ?>
 
-  <table class="forum-table forum-table-topics <?php print $classes; ?>">
-    <thead>
-      <tr>
-        <?php foreach ($header as $field => $label): ?>
-          <th class="views-field views-field-<?php print $fields[$field]; ?>">
-            <?php print $label; ?>
-          </th>
-        <?php endforeach; ?>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($rows as $count => $row): ?>
-        <tr class="<?php print implode(' ', $row_classes[$count]); ?>">
-          <?php if (empty($shadow[$count])): ?>
-            <?php foreach ($row as $field => $content): ?>
-              <?php /* To add popup from teaser in the title of the td, add: title="<?php print $teasers[$count] ?>"*/ ?>
-              <td class="views-field views-field-<?php print $fields[$field]; ?>">
-               <?php /* Extra label for stickies. */ ?>
-               <?php if ($field == 'title' && !empty($sticky[$count])): ?>
-                 <span class="sticky-label"><?php print t('Sticky:'); ?></span>
-               <?php endif; ?>
-               <?php print $content; ?>
-              </td>
-            <?php endforeach; ?>
-          <?php else: ?> 
-            <?php /* For shadow posts, we print only the icon and themed notice. */ ?>
-            <td class="views-field views-field-<?php print $fields['topic_icon']; ?>">
-              <?php print $row['topic_icon']; ?>
-            </td>
-            <td class="views-field views-field-<?php print $fields['title']; ?>" colspan="<?php print count($header)-1; ?>">
-               <?php print $shadow[$count]; ?>
-            </td>
-          <?php endif; ?>
+  <div class="table-wrapper">
+    <table class="forum-table forum-table-topics <?php print $classes; ?>">
+      <thead>
+        <tr>
+          <?php foreach ($header as $field => $label): ?>
+            <th class="views-field views-field-<?php print $fields[$field]; ?>">
+              <?php print $label; ?>
+            </th>
+          <?php endforeach; ?>
         </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <?php foreach ($rows as $count => $row): ?>
+          <tr>
+            <?php if (empty($shadow[$count])): ?>
+              <?php foreach ($row as $field => $content): ?>
+                <?php /* To add popup from teaser in the title of the td, add: title="<?php print $teasers[$count] ?>"*/ ?>
+                <td>
+                 <?php /* Extra label for stickies. */ ?>
+                 <?php if ($field == 'title' && !empty($sticky[$count])): ?>
+                   <span class="sticky-label"><?php print t('Sticky:'); ?></span>
+                 <?php endif; ?>
+                 <?php print $content; ?>
+                </td>
+              <?php endforeach; ?>
+            <?php else: ?> 
+              <?php /* For shadow posts, we print only the icon and themed notice. */ ?>
+              <td class="views-field views-field-<?php print $fields['topic_icon']; ?>">
+                <?php print $row['topic_icon']; ?>
+              </td>
+              <td class="views-field views-field-<?php print $fields['title']; ?>" colspan="<?php print count($header)-1; ?>">
+                 <?php print $shadow[$count]; ?>
+              </td>
+            <?php endif; ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
 </div>
